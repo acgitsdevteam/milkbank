901a31a07515175a79e764d46efc808e4b868c4a
meghi15 <163628626+meghi15@users.noreply.github.com>
2024-06-18 15:37:05 +0530

    state design is done 18/06/24
    state design is done  18/06/24

--- src/app/master/state/state.component.ts
+++ src/app/master/state/state.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./state.component.css']
 })
 export class StateComponent {
+  data = [
+    { state:'Andrapradesh',statecode: 'AP'},
+    { state:'Telangana',statecode: 'TS'},
+    { state:'Uttar pradesh',statecode: 'UP' },
+    { state:'Madyapradesh',statecode: 'MP'},
+    { state:'Tripura',statecode: 'TR'},
+    { state:'Uttarakhand',statecode: 'UA'},
+    
+    // Add more data as needed
+  ];
+
+  // Search functionality
+  searchTerm: string = '';
+
+  // Pagination
+  currentPage: number = 1;
+  itemsPerPage: number = 5;
+
+  // Function to add new item
+  addItem() {
+    // Implement logic to add new item here
+  }
+
 
 }
